25e44fac09665b6c321eb3d3035c8b3c77b4c004
Hristijan <hristijancocevski@gmail.com>
2022-12-20 00:32:35 +0100

    changing structure

---
+++ WebApp1/src/main/java/com/learn_spring_boot/webapp1/WebApp1Application.java
@@ -0,0 +1,12 @@
+package com.learn_spring_boot.webapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp1Application.class, args);
+    }
+}
---
+++ WebApp1/src/test/java/com/learn_spring_boot/webapp1/WebApp1ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.learn_spring_boot.webapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
