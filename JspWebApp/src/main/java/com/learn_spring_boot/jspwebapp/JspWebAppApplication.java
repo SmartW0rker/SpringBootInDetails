4a98ca6bf3d492e48fe1ee4ddf28a244e1904d7b
SmartW0rker <hristijancocevski@yahoo.com>
2022-12-23 20:40:46 +0100

    create JspWebApp

---
+++ JspWebApp/src/main/java/com/learn_spring_boot/jspwebapp/JspWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.learn_spring_boot.jspwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspWebAppApplication.class, args);
+    }
+
+}
