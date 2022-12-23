db642072daf11df8b7b65041a709954e7dff36b1
SmartW0rker <hristijancocevski@yahoo.com>
2022-12-23 22:53:14 +0100

    creating controllers and jsp pages

---
+++ JspWebApp/src/main/java/com/learn_spring_boot/jspwebapp/hello/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.learn_spring_boot.jspwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/hello-world")
+    @ResponseBody
+    public String helloWorld(){
+        return "Hello Spring Framework !";
+    }
+
+    @RequestMapping("/hello-world-html")
+    @ResponseBody
+    public String helloWorldHtml(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title> My First HTML Page</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("My first html page with body");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
+    }
+    @RequestMapping("/hello-world-jsp")
+    public String helloWorldJsp(){
+        return "sayHello";
+    }
+}
---
+++ JspWebApp/src/main/java/com/learn_spring_boot/jspwebapp/hello/LoginController.java
@@ -0,0 +1,12 @@
+package com.learn_spring_boot.jspwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
